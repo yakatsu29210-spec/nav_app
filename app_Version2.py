2f95ee7e39055437130532d0b243fd0a99130c22
yakatsu29210-spec <yakatsu29210@gmail.com>
2026-01-27 02:46:29 +0900

    Update app_Version2.py

--- app_Version2.py
+++ app_Version2.py
@@ -7,6 +7,10 @@ import numpy as np
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 # =====================================================
 # Skyfield
 # =====================================================
