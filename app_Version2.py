bea7d1e13508b3525172076d4b0dd46ff17b9491
yakatsu29210-spec <yakatsu29210@gmail.com>
2026-01-27 13:55:03 +0900

    Update app_Version2.py

--- app_Version2.py
+++ app_Version2.py
@@ -182,35 +182,38 @@ def compute_fix():
     A, b = [], []
 
     for o in obs:
-        obs_time = datetime.time.fromisoformat(o["obs_time"])
+    obs_time = datetime.time.fromisoformat(o["obs_time"])
 
-        t_obs = datetime.datetime.combine(
-            base_time.date(),
-            obs_time,
-            tzinfo=pytz.UTC
-        )
+    t_obs = datetime.datetime.combine(
+        base_time.date(),
+        obs_time,
+        tzinfo=pytz.UTC
+    )
 
-       delta_h = (t_obs - base_time).total_seconds() / 3600.0
+    delta_h = (t_obs - base_time).total_seconds() / 3600.0
 
-        lat_dr, lon_dr = dr_position(
-            lat0, lon0,
-            course, speed,
-            delta_h
-        )
+    lat_dr, lon_dr = dr_position(
+        lat0,
+        lon0,
+        course,
+        speed,
+        delta_h
+    )
 
-        t = ts.from_datetime(t_obs)
+    t = ts.from_datetime(t_obs)
 
-        Hs = float(o['deg']) + float(o['min']) / 60.0
-        Ho = Hs + dip_correction(eye) + bennett_refraction(Hs, p, temp)
+    Hs = float(o["deg"]) + float(o["min"]) / 60.0
+    Ho = Hs + dip_correction(eye) + bennett_refraction(Hs, p, temp)
 
-        star = next(s for s in STAR_CATALOG if s["id"] == o["id"])
+    star = next(s for s in STAR_CATALOG if s["id"] == o["id"])
 
-        Hc, Zn = compute_alt_az(lat_dr, lon_dr, star["star_obj"], t)
+    Hc, Zn = compute_alt_az(lat_dr, lon_dr, star["star_obj"], t)
 
-        a = (Ho - Hc) * 60.0
+    a = (Ho - Hc) * 60.0
 
-        A.append([cos(radians(Zn)), sin(radians(Zn))])
-        b.append(a)
+    A.append([cos(radians(Zn)), sin(radians(Zn))])
+    b.append(a)
+
 
     A = np.array(A)
     b = np.array(b)
