cec7a657d3c53b8063c1744a12ef726fdefc8dd2
yakatsu29210-spec <yakatsu29210@gmail.com>
2026-01-27 15:16:51 +0900

    Update app_Version2.py

--- app_Version2.py
+++ app_Version2.py
@@ -181,41 +181,43 @@ def compute_fix():
     A = []
     b = []
 
-    for o in obs:
-        obs_time = datetime.time.fromisoformat(o["obs_time"])
+for o in obs:
+    obs_time = datetime.time.fromisoformat(
+        o["obs_time"].replace("Z", "")
+    )
 
-        t_obs = datetime.datetime.combine(
-            base_time.date(),
-            obs_time,
-            tzinfo=pytz.UTC
-        )
+    t_obs = datetime.datetime.combine(
+        base_time.date(),
+        obs_time,
+        tzinfo=datetime.timezone.utc
+    )
 
-        delta_h = (t_obs - base_time).total_seconds() / 3600.0
+    # 日付跨ぎ補正
+    if t_obs < base_time:
+        t_obs += datetime.timedelta(days=1)
 
-        # DR位置
-        lat_dr, lon_dr = dr_position(
-            lat0, lon0,
-            course, speed,
-            delta_h
-        )
+    delta_h = (t_obs - base_time).total_seconds() / 3600.0
 
-        t = ts.from_datetime(t_obs)
+    # DR位置
+    lat_dr, lon_dr = dr_position(
+        lat0, lon0,
+        course, speed,
+        delta_h
+    )
 
-        # 観測高度
-        Hs = float(o["deg"]) + float(o["min"]) / 60.0
-        Ho = Hs + dip_correction(eye) + bennett_refraction(Hs, p, temp)
+    t = ts.from_datetime(t_obs)
 
-        # 恒星取得
-        star = next(s for s in STAR_CATALOG if s["id"] == o["id"])
+    Hs = float(o["deg"]) + float(o["min"]) / 60.0
+    Ho = Hs + dip_correction(eye) + bennett_refraction(Hs, p, temp)
 
-        # 計算高度・方位角
-        Hc, Zn = compute_alt_az(lat_dr, lon_dr, star["star_obj"], t)
+    star = next(s for s in STAR_CATALOG if s["id"] == o["id"])
+    Hc, Zn = compute_alt_az(lat_dr, lon_dr, star["obj"], t)
 
-        # 截距（NM）
-        a = (Ho - Hc) * 60.0
+    a = (Ho - Hc) * 60.0
 
-        A.append([cos(radians(Zn)), sin(radians(Zn))])
-        b.append(a)
+    A.append([cos(radians(Zn)), sin(radians(Zn))])
+    b.append(a)
+
 
     A = np.array(A)
     b = np.array(b)
